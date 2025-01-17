fa6877a234960cdda68f9470fa7ffcba0075bcb3
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2025-01-17 19:45:45 +0200

    Add Firebase integration and update dashboard view structure

--- lib/features/dashboard/views/dash_board_view.dart
+++ lib/features/dashboard/views/dash_board_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub_dash_board/features/dashboard/views/widgets/dash_board_view_body.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
   static const routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: DashBoardViewBody(),
+    );
   }
 }
---
+++ lib/features/dashboard/views/widgets/dash_board_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashBoardViewBody extends StatelessWidget {
+  const DashBoardViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
